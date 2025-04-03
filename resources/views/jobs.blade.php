4f915d3d8e988f1db5ce896955464b540ba1a43b
Alvin Rapada <alvnrapada@gmail.com>
2025-04-03 18:34:11 +0800

    Day 13: - Lazy Loading Explained and How to do Eager Loading - Updating Job Card UI - How to Disable Lazy Loading on AppServiceProvider - Installing laravel-debugbar

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,14 +1,13 @@
 <x-layout>
     <x-slot:page_title>Job Listings</x-slot:heading>
 
-    <ul>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
     @foreach ($jobs as $job)
-        <li>
-            <a class="hover:underline" href="/jobs/{{ $job['id'] }}">
-                <strong>{{ $job['title'] }}</strong>
-                Pays {{ $job['salary'] }} per year.
+            <a class="block p-6 rounded-md shadow-md group" href="/jobs/{{ $job['id'] }}">
+                <p class="text-xs mb-4 font-bold text-blue-600">{{ $job->employer->name }}</p>
+                <h1 class="text-lg group-hover:underline font-bold">{{ $job['title'] }}</h1>
+                <p class="text-sm">Pays {{ $job['salary'] }} per year.</p>
             </a>
-        </li>
     @endforeach
-    </ul>
+    </div>
 </x-layout>
