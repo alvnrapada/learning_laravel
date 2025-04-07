ad2124a87170ed49767a566d1cc4459a6d54cc30
Alvin Rapada <alvnrapada@gmail.com>
2025-04-07 16:26:15 +0800

    Day 25: Jobs / Queue - dispatch() - queue() - php artisan queue:work

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -62,7 +62,7 @@ class JobController extends Controller
             'employer_id' => request('employer_id')
         ]);
         
-        Mail::to($job->employer->user)->send(new JobPosted($job));
+        Mail::to($job->employer->user)->queue(new JobPosted($job));
     
         return redirect('/jobs');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+
+Route::get('/mail', function () {
+   $job = Job::first();
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::view('/', 'home', [
     'greeting' => 'Welcome Home!'
